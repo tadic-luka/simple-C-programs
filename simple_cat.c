b2b2fd0f4b40eb0f51348990e19bb92ac6fb5136
leroy_brown <luka.tadic@fer.hr>
2017-05-14 17:21:47 +0200

    refactored

--- simple_cat.c
+++ simple_cat.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -19,28 +18,20 @@ void read_fd(int fd)
 		err(1, "malloc");	
 	}
 	int ret;
-	printf("%d\n", fd_stat.st_blksize);
 	while((ret = read(fd, buf, fd_stat.st_blksize))) {
 		if(ret == -1) {
 			err(1, "read");
 		}
 		fwrite(buf, 1, ret, stdout);
 	}
-
-}
-void new_handler(int sig)
-{
-	puts("primio signal");
 }
 int main(int argc, char *argv[])
 {
 	argv++;
-	signal(SIGINT, new_handler);	
+	(void) argc;
 	
-	struct stat fd_stat;
 	while(*argv) {
-		printf("%s\n", *argv);
-		if(!memcmp(*argv, "-", 1)) {
+		if(**argv == '-') {
 			read_fd(0);
 		} else {
 			int fd = open(*argv, 0);
