988914d0c7f9035c5bbd5baef4cb44b6cfd8ae1c
leroy_brown <luka.tadic@fer.hr>
2017-05-23 21:37:04 +0200

    added program option

--- simple_tee.c
+++ simple_tee.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -30,11 +31,18 @@ void tee(int fd)
 int main(int argc, char *argv[])
 {
 	++argv;
+	int flags = O_CREAT | O_WRONLY;
 	if(argc < 2) {
 		fprintf(stderr, "Usage: simple_tee [-a] file");
 	}
 	while(*argv) {
-		int fd = open(*argv, O_CREAT | O_WRONLY, S_IRUSR | S_IRGRP | S_IROTH);
+		if(memcmp(*argv, "-a", 2) == 0) {
+			/*flags &= ~O_CREAT;*/
+			flags |= O_APPEND;
+			++argv;
+			continue;
+		}
+		int fd = open(*argv, flags, S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH);
 		if(fd == -1) {
 			err(1, "open %s", *argv);
 		}
